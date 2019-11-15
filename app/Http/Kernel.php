2a22ccd9e72f5e17de313c034340a87104e7c5e6
Andi Muhammad Chaerul Hafidz <chaerulhafidz@gmail.com>
2019-11-15 07:31:24 +0700

    updated enable CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
     ];
 
     /**
@@ -60,6 +61,7 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cors' => \App\Http\Middleware\Cors::class, // <-- add this line
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/arah_angin', array('middleware' => 'cors', 'uses' => 'API\AWSController@arah_angin'));
+Route::get('/suhu', array('middleware' => 'cors', 'uses' => 'API\AWSController@suhu'));
+Route::get('/kelembaban', array('middleware' => 'cors', 'uses' => 'API\AWSController@kelembaban'));
+Route::get('/tekanan_udara', array('middleware' => 'cors', 'uses' => 'API\AWSController@tekanan_udara'));
+Route::get('/intensitas_cahaya', array('middleware' => 'cors', 'uses' => 'API\AWSController@intensitas_cahaya'));
+Route::get('/kualitas_udara', array('middleware' => 'cors', 'uses' => 'API\AWSController@kualitas_udara'));
+Route::get('/ketinggian', array('middleware' => 'cors', 'uses' => 'API\AWSController@ketinggian'));
+Route::get('/kondisi_cuaca', array('middleware' => 'cors', 'uses' => 'API\AWSController@kondisi_cuaca'));
 
-Route::get('/arah_angin', 'API\AWSController@arah_angin');
-Route::get('/suhu', 'API\AWSController@suhu');
-Route::get('/kelembaban', 'API\AWSController@kelembaban');
-Route::get('/tekanan_udara', 'API\AWSController@tekanan_udara');
-Route::get('/intensitas_cahaya', 'API\AWSController@intensitas_cahaya');
-Route::get('/kualitas_udara', 'API\AWSController@kualitas_udara');
-Route::get('/kondisi_cuaca', 'API\AWSController@kondisi_cuaca');
-Route::get('/ketinggian', 'API\AWSController@ketinggian');
+//Route::get('/arah_angin', 'API\AWSController@arah_angin');
+//Route::get('/suhu', 'API\AWSController@suhu');
+//Route::get('/kelembaban', 'API\AWSController@kelembaban');
+//Route::get('/tekanan_udara', 'API\AWSController@tekanan_udara');
+//Route::get('/intensitas_cahaya', 'API\AWSController@intensitas_cahaya');
+//Route::get('/kualitas_udara', 'API\AWSController@kualitas_udara');
+//Route::get('/kondisi_cuaca', 'API\AWSController@kondisi_cuaca');
+//Route::get('/ketinggian', 'API\AWSController@ketinggian');
